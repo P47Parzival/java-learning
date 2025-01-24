6bc284c6b2644e6104fa07853162686021f6cb1c
Dhruv Mali <158600867+P47Parzival@users.noreply.github.com>
2025-01-24 18:43:21 +0530

    Update Functions.java

--- Functions.java
+++ Functions.java
@@ -20,3 +20,22 @@ public static void main(String args[]) {
         return 0;
     }
 
+// Fibonacci series code
+public static void main(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the digits you want in series: ");
+        int a = sc.nextInt();
+        System.out.println("Fibonnaci series is: ");
+        fibo(a);
+        sc.close();
+    }
+    public static void fibo(int a){
+        int first = 0, second = 1;
+        System.out.print(first + " " + second + " ");
+        for(int i = 3; i <= a; i++){
+            int next = first + second;
+            System.out.print(next + " ");
+            first = second;
+            second = next;
+        }
+    }
