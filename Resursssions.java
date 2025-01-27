a3fae0176f328bce820117b14631a198e2df18ec
Dhruv Mali <158600867+P47Parzival@users.noreply.github.com>
2025-01-27 22:02:35 +0530

    Update Resursssions.java

--- Resursssions.java
+++ Resursssions.java
@@ -1,1 +1,12 @@
-
+//print 1 to 5
+    // public static void main(String args[]){
+    //     int n = 1;
+    //     printnumbers(n);
+    // }
+    // public static void printnumbers(int n){
+    //     if(n==6){
+    //         return;
+    //     }
+    //     System.out.println(n);
+    //     printnumbers(n+1);
+    // }
