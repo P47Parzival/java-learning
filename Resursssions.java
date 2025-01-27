26fb28d30e88f4891fb7b90a3ee8ba0735eef211
Dhruv Mali <158600867+P47Parzival@users.noreply.github.com>
2025-01-27 22:03:42 +0530

    Update Resursssions.java

--- Resursssions.java
+++ Resursssions.java
@@ -12,24 +12,24 @@
     }
 
 //print sum of n natural numbers
-    // public static void printsum(int i, int n, int sum){
-    //     if(i == n){
-    //         sum = sum + i;
-    //         System.out.println(sum);
-    //         return; 
-    //     }
-    //     sum = sum + i;
-    //     printsum(i+1, n, sum);
-    // }
-    // public static void main(String args[]){
-    //     Scanner sc = new Scanner(System.in);
-    //     System.out.print("Enter the number till wich you want sum for: ");
-    //     int n = sc.nextInt();
-    //     printsum(1, n, 0);
-    //     sc.close();
-    // }
+    public static void printsum(int i, int n, int sum){
+        if(i == n){
+            sum = sum + i;
+            System.out.println(sum);
+            return; 
+        }
+        sum = sum + i;
+        printsum(i+1, n, sum);
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number till wich you want sum for: ");
+        int n = sc.nextInt();
+        printsum(1, n, 0);
+        sc.close();
+    }
 
-    //factorial of number n 
+//factorial of number n 
     public static void main(String args[]){
         int n = 5;
         System.out.println(factorial(n));
