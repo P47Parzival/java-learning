15491436984ab0ae3893ae7b53065f4f91b55dd8
Dhruv Mali <158600867+P47Parzival@users.noreply.github.com>
2025-01-28 15:52:30 +0530

    Update Resursssions.java

--- Resursssions.java
+++ Resursssions.java
@@ -75,3 +75,28 @@
         System.out.print(name.charAt(indx));
         reverse(name, indx-1);
     }
+
+//find first and last occurence of an element in a string
+    public static int first = -1;
+    public static int last = -1;
+    public static void main(String args[]){
+        String str = "abaacdaefaah";
+        char element = 'a';
+        occurence(str, 0, element);
+    }
+    public static void occurence(String str, int idx, char element){
+        if(idx == str.length()){
+            System.out.println(first);
+            System.out.println(last);
+            return;
+        }
+        char currentchar = str.charAt(idx);
+        if(currentchar == element){
+            if(first == -1){
+                first = idx;
+            }else{
+                last = idx;
+            }
+        }
+        occurence(str, idx+1, element);
+    }
